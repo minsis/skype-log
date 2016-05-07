710dc8a09350b340dd5d1189661c1657ae348a46
Dennis Whitney <denniswhitney@irunasroot.com>
2016-05-07 15:12:37 -0500

    Made python2 and python3 friendly, cleaned up code

--- run.py
+++ run.py
@@ -1,3 +1,6 @@
 from skype_log import Skypper
 
 my_log = Skypper()
+
+print(my_log.get_skype_user_path())
+print(my_log.get_database_path())
--- skype_log/skype_log.py
+++ skype_log/skype_log.py
@@ -2,7 +2,7 @@ import sqlite3
 from platform import system
 from os.path import exists, isdir, join
 from os import environ, listdir
-import ErrHandle as ErrHandle
+from .ErrHandle import *
 
 class Skypper:
 
@@ -11,30 +11,30 @@ class Skypper:
 
         self.os_platform = system()
         self.home_dir = environ["HOME"]
+        self.skype_user_path = None
 
         if skype_path is not None:
             self.skype_path = skype_path
         elif self.os_platform == "Linux":
-            self.skype_path = home_dir + "/.Skype/"
+            self.skype_path = self.home_dir + "/.Skype/"
         elif self.os_platform == "Windows":
             self.skype_path = environ["APPDATA"] + "\\Skype\\"
-        else:
-            if exists(self.skype_path): pass
 
-        if not exists(self.skype_path): ErrHandle.no_path()
-
-        self.get_skype_user_path()
+        if not exists(self.skype_path): no_path()
 
     def get_skype_user_path(self):
-        """Get the Skype user folder, or give a list if multiple found"""
+        """Get list of Skype user folder(s)"""
 
         default_skype_folders = ["Content", "DataRv", "My Skype Received Files", "RootTools", "shared_dynco", "shared_httpfe"]
 
         if self.skype_path == None: self.__init__()
 
-        skype_users_dir = ( [ directory for directory in listdir(self.skype_path)
+        self.skype_user_path = ( [ directory for directory in listdir(self.skype_path)
                             if isdir(join(self.skype_path, directory)) and directory not in default_skype_folders ] )
 
-    def get_database_path():
+        return(self.skype_user_path)
+
+    def get_database_path(self):
         """Get the path of the SQLite database for Skype"""
-        pass
+
+        if self.skype_user_path == None: self.get_skype_user_path()
