a84ce40f8e4664f6c34549bd6920c7cd2ddf3349
Dennis Whitney <denniswhitney@irunasroot.com>
2016-05-07 13:22:10 -0400

    added function to get list of user directories

--- skype_log/skype_log.py
+++ skype_log/skype_log.py
@@ -1,15 +1,16 @@
 import sqlite3
 from platform import system
-from os.path import exists
-from os import environ
+from os.path import exists, isdir, join
+from os import environ, listdir
 import ErrHandle as ErrHandle
 
 class Skypper:
 
     def __init__(self, skype_path=None):
+        """Initilize variables looking for the OS type and path of Skype"""
+
         self.os_platform = system()
         self.home_dir = environ["HOME"]
-        self.default_skype_folders = ["DataRv", "shared_dynco", "shared_httpfe"]
 
         if skype_path is not None:
             self.skype_path = skype_path
@@ -21,3 +22,19 @@ class Skypper:
             if exists(self.skype_path): pass
 
         if not exists(self.skype_path): ErrHandle.no_path()
+
+        self.get_skype_user_path()
+
+    def get_skype_user_path(self):
+        """Get the Skype user folder, or give a list if multiple found"""
+
+        default_skype_folders = ["Content", "DataRv", "My Skype Received Files", "RootTools", "shared_dynco", "shared_httpfe"]
+
+        if self.skype_path == None: self.__init__()
+
+        skype_users_dir = ( [ directory for directory in listdir(self.skype_path)
+                            if isdir(join(self.skype_path, directory)) and directory not in default_skype_folders ] )
+
+    def get_database_path():
+        """Get the path of the SQLite database for Skype"""
+        pass
